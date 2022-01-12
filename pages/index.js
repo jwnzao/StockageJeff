c6b01d80a45812e7b7f5e82ce0a899de865f9135
= <jeff.aprentiv@gmail.com>
2022-01-12 15:33:42 +0100

    envoie du stockage

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Heading, Page, View } from "@shopify/polaris";
 import ProduitState from "./Components/ProduitState.js";
+//import RT_Footer from "./Components/Footer.js";
 
 var initProds = [];
-for(var i = 1; i <= 10; i++)
-	initProds.push({
-		id: i,
-		name: "Produit " + i,
-		quantity: i,
-		description: "Description du Produit " + i,
-	});
-
+for (var i = 1; i <= 10; i++)
+  initProds.push({
+    id: i,
+    name: "Produit " + i,
+    quantity: i,
+    description: "Description du Produit " + i,
+  });
 
 const Index = () => (
   <Page>
-	<ProduitState initProduits={initProds} />
+    {/* <head>
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css" rel="stylesheet"/>
+
+	</head>
+
+
+	<body>
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+
+	</body> */}
+    <ProduitState initProduits={initProds} />
   </Page>
 );
 
 export default Index;
-
